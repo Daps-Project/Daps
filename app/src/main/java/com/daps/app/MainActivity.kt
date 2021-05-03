73e39daf3e685faa6914d0ac9638f73825b8a2f4
jamaliethenor <jamalie10>
2021-05-02 22:19:59 -0400

    Add Google Places API handle  https://developers.google.com/maps/documentation/places/android-sdk/start

--- app/src/main/java/com/daps/app/MainActivity.kt
+++ app/src/main/java/com/daps/app/MainActivity.kt
@@ -2,10 +2,16 @@ package com.daps.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.libraries.places.api.Places
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // https://developers.google.com/maps/documentation/places/android-sdk/start
+        Places.initialize(applicationContext, "AIzaSyDrzDcMBvxz0DSFIhm0vrzRDAaZi1VOOjs")
+        // Create a new PlacesClient instance
+        val placesClient = Places.createClient(this)
     }
 }
